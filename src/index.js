c8ec0a2654a6412887694121b9cc2ea00270d973
timurkravchenko <34355@iitu.edu.kz>
2025-01-29 12:16:18 +0500

    step2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello Samat');
+import React from "react";
+import {createRoot} from "react-dom/client";
+//const element = React.createElement('hi', null, 'Hello React World');
+const element = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Install React</li>
+            <li>Study React</li>
+            <li>Use React</li>
+            <li>Build React App</li>
+        </ul>
+    </div>
+);
+const root = createRoot(document.getElementById("root"));
+root.render(element)
