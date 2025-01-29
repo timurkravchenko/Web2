2d540f5eaaaca6df31c96e35ac6966660ced02f0
timurkravchenko <34355@iitu.edu.kz>
2025-01-29 14:28:28 +0500

    step5

--- src/index.js
+++ src/index.js
@@ -2,47 +2,21 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 
 
+import AppHeader from "./Components/app-header";
+import SearchPanel from "./Components/search-panel";
+import TodoList from "./Components/todo-list";
 
-const TodoList = () => {
-    const items = ['Install React', 'Study React', 'Use React', 'Build React App'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-            <li>{items[2]}</li>
-            <li>{items[3]}</li>
-        </ul>
-);
-};
-const AppHeader = () =>{
-    return <h1>My Todo List</h1>;
-};
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-    // return <input placeholder="search" />;
-    return <input
-        style = {searchStyle}
-        placeholder= {searchText}
-        disabled={true} />;
-}
 const App = () => {
-    const value = '<script>alert ("")</script>';
-    // const isLoggedIn = false;
-    // const loginBox = <span>Log in to the system</span>;
-    // const welcomeBox = <span>Welcome Back</span>;
+
     return (
         <div>
-            {/*{ isLoggedIn ? null : loginBox }*/}
-            { value }
+
             <AppHeader />
             <SearchPanel />
             <TodoList/>
         </div>
     );
 }
-// const element = <App />;
+
 const root = createRoot(document.getElementById("root"));
 root.render(<App />)
