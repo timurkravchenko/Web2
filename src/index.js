871debbdfc49ea44ffefbb32b46711ca36da82f1
timurkravchenko <34355@iitu.edu.kz>
2025-02-05 10:16:23 +0500

    step 13 Preparing for Todo List Project status filterg

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-
+import ReactDOM from 'react-dom';
 
 import AppHeader from "./Components/app-header";
 import SearchPanel from "./Components/search-panel";
 import TodoList from "./Components/todo-list";
 import ItemStatusFilter from "./components/item-status-filter";
 
+import './index.css';
 
-const App = () => {
-    const todoData = [
+
+    const App = () => {
+
+
+        const todoData = [
+
         { label: 'Drink Coffee', important: false,  id: 1},
-        { label: 'Implement React Application', important: true, id: 2 },
+        { label: 'React Application', important: true, id: 2 },
         { label: 'Make notes from your stud', important: false, id: 3 },
         { label: 'Do not drink alchohol', important: false, id: 4 },
-    ];
+];
 
-    return (
-        <div>
-
-            <AppHeader/>
-            <SearchPanel />
-            <TodoList todos =  {todoData} />
-            <ItemStatusFilter />
+    return  (
+    <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
         </div>
+            <TodoList todos={todoData} />
+    </div>
     );
-}
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />)
+};
+ReactDOM.render(<App />,
+ document.getElementById('root'));
+
+
