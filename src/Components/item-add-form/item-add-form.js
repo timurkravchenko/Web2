64163a2e500c91e01aa064b8425f8c189628b38e
timurkravchenko <34355@iitu.edu.kz>
2025-02-19 10:39:50 +0500

    step 32 Working with form add new Item with type new list item

--- src/Components/item-add-form/item-add-form.js
+++ src/Components/item-add-form/item-add-form.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import './item-add-form.css';
 export default class ItemAddForm extends  Component {
+    state = {
+        label: ''
+    };
+    onLabelChange = (e) => {
+        // console.log('rgrggn');
+        // console.log(e.target.value)
+        this.setState({
+            label: e.target.value
+        });
+    };
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    };
     render() {
         return (
-            <div className={ "item-add-form" }>
+            <form className={ "item-add-form d-flex" }
+                  onSubmit={this.onSubmit}>
+                <input type={"text"}
+                       className={"form-control"}
+                       onChange = {this.onLabelChange}
+                       placeholder={"What need to be done"} />
                 <button
-                    className={ "btn btn-outline-secondary" }
-                    onClick={() => this.props.onItemAdded('Hello World')}>
-                    Add Item
+                    className={ "btn btn-outline-secondary" } >
+            Add Item
                 </button>
-            </div>
+            </form>
         )
     }
 }
